f41d035f8c4bb78fab5005689c1e6b668f88037f
Lucas Calegario <lucas.c.calegario@outlook.com>
2026-06-24 15:01:18 -0300

    first commit

---
+++ neves_be/conftest.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from neves_be.users.tests.factories import UserFactory
+
+if TYPE_CHECKING:
+    from neves_be.users.models import User
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory.create()
